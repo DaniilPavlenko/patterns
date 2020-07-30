345835b9c8aa0ad41be3dd8a13c26055421ad7ce
Daniil Pavlenko <pavlenko.develop@gmail.com>
2020-07-30 21:05:11 +0300

    Add ForecastDisplay

--- src/observer/weatherstation/MainWeatherStation.kt
+++ src/observer/weatherstation/MainWeatherStation.kt
@@ -1,6 +1,7 @@
 package observer.weatherstation
 
 import observer.weatherstation.display.CurrentConditionDisplay
+import observer.weatherstation.display.ForecastDisplay
 
 object MainWeatherStation {
 
@@ -9,6 +10,7 @@ object MainWeatherStation {
         val weatherData = WeatherData()
 
         val currentConditionDisplay = CurrentConditionDisplay(weatherData)
+        val forecastDisplay = ForecastDisplay(weatherData)
 
         weatherData.setMeasurement(90F, 23F, 49.6F)
         weatherData.setMeasurement(23F, 25F, 63.1F)
